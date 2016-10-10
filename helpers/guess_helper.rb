332cdb920a1ac1432b5e7b64976a3010a24d197b
K Ołdakowski <krz.oldakowski@gmail.com>
2016-10-10 16:20:48 +0200

    add guess number limit

--- helpers/guess_helper.rb
+++ helpers/guess_helper.rb
@@ -1,13 +1,42 @@
-def check_guess(number)
-  if number + 5 < settings.secret_number
-    guess = "Way too low!" 
-  elsif number < settings.secret_number
-    guess = "Too low!"
-  elsif number == settings.secret_number
-    guess = "Correct!"
-  elsif number - 5 > settings.secret_number
-    guess = "Way too high!"
-  elsif number > settings.secret_number
-    guess = "Too high!"
+class Tries
+  @@number = 0
+  def self.number
+    @@number
   end
+  def self.increase
+    @@number += 1
+  end
+  def self.reset
+    @@number = 0
+  end
+end
+
+
+
+def check_guess(guess)
+  Tries.increase  
+  if Tries.number == 5
+    reset_game
+    answer = "Wrong, reached limit of guesses! New game!"
+  elsif guess + 5 < settings.secret_number
+    answer = "Way too low" 
+  elsif guess < settings.secret_number
+    answer = "Too low!"
+  elsif guess - 5 > settings.secret_number
+    answer = "Way too high!"
+  elsif guess > settings.secret_number
+    answer = "Too high!"
+  elsif guess == settings.secret_number
+    reset_game
+    answer = "Correct! New game!"
+  end
+end
+
+def generate_number
+  rand(100)
+end
+
+def reset_game
+  settings.secret_number = generate_number
+  Tries.reset
 end
--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './helpers/guess_helper'
 
-set :secret_number, rand(100)
+set :secret_number, generate_number
 
 get '/' do
-  guess = check_guess(params["guess"].to_i) if params["guess"]  
-  erb :index, locals: { random_number: settings.secret_number, guess: guess }
+  params["guess"] ? answer = check_guess(params["guess"].to_i) : Tries.reset
+  erb :index, locals: { random_number: settings.secret_number, 
+                        answer: answer,
+                        tries: Tries.number}
 end
